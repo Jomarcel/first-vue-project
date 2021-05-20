8664972d8269ea058664a4ea44173624c514417a
Jomarcel Nguyen <jomarcelnguyen@Jomarcels-MBP.localdomain>
2021-05-20 10:39:38 +1000

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "jo",
+  props: {
+    title: {
+      type: String,
+      default: "yoyo",
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
